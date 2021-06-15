6860094520821a2203187f0919777d9e899f3200
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-15 21:33:19 +0530

    Refactor:created static method

--- src/main/java/bridgelabz/AlgorithmProblems/PrimeNumbersInRange.java
+++ src/main/java/bridgelabz/AlgorithmProblems/PrimeNumbersInRange.java
@@ -10,14 +10,20 @@ package bridgelabz.AlgorithmProblems;
 import java.util.Scanner;
 
 public class PrimeNumbersInRange {
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a, b, i, j, flag;
+		int a, b;
 		System.out.printf("Enter lower bound of the interval: ");
 		a = sc.nextInt();
 		System.out.printf("\nEnter upper bound of the interval: ");
 		b = sc.nextInt();
 		System.out.printf("\nPrime numbers between %d and %d are: ", a, b);
+		primeNumbersInRange(a, b);
+	}
+
+	private static void primeNumbersInRange(int a, int b) {
+		int i, j, flag;
 		for (i = a; i <= b; i++) {
 			if (i == 1 || i == 0)
 				continue;
